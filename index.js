a045aface039330cdf56c8d168dbd9b84d85b914
Peera Aronrat <peelaxv@gmail.com>
2026-01-21 14:44:45 +0700

    feat: add /cart route

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get("/about", (req, res) => {
 
 app.get("/info", (req, res) => {
   res.status(200).json({ message: "info page" });
+});'
+
+app.get("/cart", (req, res) => {
+  res.status(200).json({ message: "cart page" });
 });
 
 app.listen(PORT, () => {
